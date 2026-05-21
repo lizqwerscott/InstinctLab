cda7e4db348ab133e24949300990be62f78346fd
jie wang <1261620820@qq.com>
2026-05-21 11:55:46 +0800

    feat(parkour): Increase flat patch count to 120

--- source/instinctlab/instinctlab/tasks/parkour/config/parkour_env_cfg.py
+++ source/instinctlab/instinctlab/tasks/parkour/config/parkour_env_cfg.py
@@ -177,7 +177,7 @@ ROUGH_TERRAINS_CFG = TerrainGeneratorCfg(
             ),
             flat_patch_sampling={
                 "target": FlatPatchSamplingCfg(
-                    num_patches=50,
+                    num_patches=120,
                     patch_radius=[0.05, 0.10, 0.15, 0.20],
                     max_height_diff=0.05,
                     x_range=(5.7, 5.7),
@@ -205,7 +205,7 @@ ROUGH_TERRAINS_CFG = TerrainGeneratorCfg(
             ),
             flat_patch_sampling={
                 "target": FlatPatchSamplingCfg(
-                    num_patches=50,
+                    num_patches=120,
                     patch_radius=[0.05, 0.10, 0.15, 0.20],
                     max_height_diff=0.05,
                     x_range=(5.7, 5.7),
