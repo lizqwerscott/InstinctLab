b24fd81bef0e9efc488e0e33eee3fa284610cc1d
draco30 <liutaoql30@gmail.com>
2026-06-08 17:57:23 +0800

    fix: fix config param

--- source/instinctlab/instinctlab/tasks/parkour/config/g1/agents/instinct_rl_amp_cfg.py
+++ source/instinctlab/instinctlab/tasks/parkour/config/g1/agents/instinct_rl_amp_cfg.py
@@ -183,6 +183,18 @@ class AmpAlgoStudentCfg(InstinctRlPpoAlgorithmCfg):
     teacher_logdir = os.path.expanduser(
         "~/Data/instinctlab_logs/instinct_rl/g1_perceptive_shadowing/20260111_103654_g1Perceptive_4MotionsKneelClimbStep1_concatMotionBins__GPU0_from20260108_032900"
     )
+    value_loss_coef = 1.0
+    use_clipped_value_loss = True
+    clip_param = 0.2
+    entropy_coef = 0.006
+    denoise_loss_coef = 0.1
+    num_learning_epochs = 5
+    num_mini_batches = 4
+    learning_rate = 1.0e-3
+    schedule = "adaptive"
+    gamma = 0.99
+    lam = 0.95
+    desired_kl = 0.01
 
     max_grad_norm = 1.0
 
